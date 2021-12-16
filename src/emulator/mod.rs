ad148a6f7b0cf2788081d7976ba5e29a6b74d8de
Gil Mendes <gil00mendes@gmail.com>
2021-12-16 17:06:29 +0000

    implement event channels

--- src/emulator/mod.rs
+++ src/emulator/mod.rs
@@ -1,10 +1,57 @@
+use std::{
+    sync::mpsc::{Receiver, Sender},
+    time::Duration,
+};
+
 use self::{
     cpu::CPU,
-    memory::{DeviceRegister, Memory, MEMORY_SIZE},
+    memory::{Memory, MEMORY_SIZE},
 };
 
 mod cpu;
 mod memory;
+
+/// Device registers
+#[derive(Debug)]
+pub enum DeviceRegister {
+    KeyboardStatus = 0xFE00,
+    KeyboardData = 0xFF02,
+    DisplayStatus = 0xFE04,
+    DisplayData = 0xFF06,
+    MachineControl = 0xFFFE,
+}
+
+/// Structure to help managing events
+pub struct Events {
+    /// Emulator sender
+    emu_tx: Sender<(DeviceRegister, u16)>,
+    /// Emulator receiver
+    emu_rx: Receiver<(DeviceRegister, u16)>,
+}
+
+impl Events {
+    pub fn new(
+        emu_tx: Sender<(DeviceRegister, u16)>,
+        emu_rx: Receiver<(DeviceRegister, u16)>,
+    ) -> Self {
+        Self { emu_tx, emu_rx }
+    }
+
+    /// Send an event to the outside world
+    pub fn send(&mut self, event: DeviceRegister, data: u16) {
+        self.emu_tx.send((event, data)).unwrap()
+    }
+
+    /// Blocks until receives an event from the outside world
+    pub fn receive(&self) -> (DeviceRegister, u16) {
+        self.emu_rx.recv().unwrap()
+    }
+
+    /// Blocks until receives an event from the outside world, but timeouts if it takes more than 10ms
+    pub fn receive_timeout(&self) -> Option<(DeviceRegister, u16)> {
+        self.emu_rx.recv_timeout(Duration::from_millis(10)).ok()
+    }
+}
 
 pub struct Emulator {
     memory: Memory,
@@ -13,13 +60,14 @@ pub struct Emulator {
 
 impl Emulator {
     pub fn new(binary_data: Vec<u8>) -> Self {
+        // first 16 bit entry is the base address here the binary must be loaded. This is also the initial address where
+        // the program counter must point to.
+        let mut address = (binary_data[0] as u16) << 8 | (binary_data[1] as u16);
+
         let mut emulator = Self {
             memory: Memory::new(),
-            cpu: CPU::new(),
+            cpu: CPU::new(address),
         };
-
-        // first 16 bit entry is the base address here the binary must be loaded
-        let mut address = (binary_data[0] as u16) << 8 | (binary_data[1] as u16);
 
         // load the rest of the binary on the memory
         let mut i = 2;
@@ -35,14 +83,39 @@ impl Emulator {
         emulator
     }
 
+    /// Deal I/O events from CPU and from the outside world
+    fn check_events(&mut self, events: &mut Events) {
+        // check if there is a new event from the outside would
+        match events.receive_timeout() {
+            Some((DeviceRegister::KeyboardData, val)) => {
+                self.memory.write(DeviceRegister::KeyboardData as u16, val);
+                self.memory.write(DeviceRegister::KeyboardStatus as u16, 1);
+            }
+            Some((DeviceRegister::DisplayStatus, _)) => {
+                self.memory
+                    .write(DeviceRegister::DisplayStatus as u16, 1 << 15);
+            }
+            _ => {}
+        }
+    }
+
     /// Initiate the execution loop
-    pub fn execute(&mut self) {
-        // before start executing we need to enable the machine
+    pub fn execute(
+        &mut self,
+        emu_tx: Sender<(DeviceRegister, u16)>,
+        emu_rx: Receiver<(DeviceRegister, u16)>,
+    ) {
+        // create a event manager to deal with the events between CPU and World
+        let mut events = Events::new(emu_tx, emu_rx);
+
+        // before start executing we need to enable the machine and tell that we can write chars to the screen
         self.memory
-            .write(DeviceRegister::MachineControl as u16, 0xF);
+            .write(DeviceRegister::MachineControl as u16, 1 << 15);
+        self.memory
+            .write(DeviceRegister::DisplayStatus as u16, 1 << 15);
 
         loop {
-            self.cpu.next_tick(&mut self.memory);
+            self.cpu.next_tick(&mut self.memory, &mut &mut events);
 
             if self.cpu.get_registers().pc >= MEMORY_SIZE as u16 {
                 println!("Emulator: out of memory bound");
@@ -53,6 +126,12 @@ impl Emulator {
             if self.memory.read(DeviceRegister::MachineControl as u16) == 0 {
                 break;
             }
+
+            // on each cycle check if there is some event to be sent
+            self.check_events(&mut events);
         }
+
+        // notify that the machine was shutdown
+        events.send(DeviceRegister::MachineControl, 1 << 15);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
+use std::io::{stdin, stdout, Write};
+use std::sync::mpsc::{self, Receiver, Sender};
+use std::thread;
+use std::time::Duration;
 use std::{fs::File, io::Read, path::Path};
 
 use clap::{App, Arg, ArgMatches};
-use emulator::Emulator;
+use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
+use emulator::{DeviceRegister, Emulator};
 
 mod emulator;
 
@@ -33,6 +38,45 @@ fn read_room<P: AsRef<Path>>(path: P) -> Vec<u8> {
     file_buffer
 }
 
+fn handle_io_events(ui_tx: Sender<(DeviceRegister, u16)>, ui_rx: Receiver<(DeviceRegister, u16)>) {
+    let stdin = stdin();
+    let stdin = stdin.lock();
+    let mut bytes = stdin.bytes();
+
+    'main: loop {
+        // check if there is a new event to be processed
+        let event = ui_rx.recv().unwrap();
+
+        match event {
+            (DeviceRegister::DisplayData, ch) => {
+                print!("{}", ch as u8 as char);
+                stdout().flush().unwrap();
+
+                // inform the CPU that we are ready to receive a new char
+                ui_tx
+                    .send((DeviceRegister::DisplayStatus, 1 << 15))
+                    .unwrap();
+            }
+            (DeviceRegister::KeyboardStatus, _) => {
+                // disable canonical and echo modes
+                enable_raw_mode().unwrap();
+
+                let ch = bytes.next().unwrap().unwrap() as u16;
+                ui_tx.send((DeviceRegister::KeyboardData, ch)).unwrap();
+
+                // restore default terminal settings
+                disable_raw_mode().unwrap();
+            }
+            (DeviceRegister::MachineControl, _) => {
+                break 'main;
+            }
+            _ => {
+                thread::sleep(Duration::from_millis(30));
+            }
+        }
+    }
+}
+
 fn main() {
     // build command line and get the matched arguments
     let matches = build_command_line();
@@ -41,7 +85,17 @@ fn main() {
     let rom_path = matches.value_of("ROM").unwrap();
     let rom_data = read_room(rom_path);
 
+    // create a new messaging passing channel to get notified about I/O events. Since we need bidirectional
+    // communication we have two two different channels.
+    let (emu_tx, ui_rx) = mpsc::channel::<(DeviceRegister, u16)>();
+    let (ui_tx, emu_rx) = mpsc::channel::<(DeviceRegister, u16)>();
+
+    // start UI thread
+    let th_handler = thread::spawn(move || handle_io_events(ui_tx, ui_rx));
+
     // Build and start the emulator
     let mut emulator = Emulator::new(rom_data);
-    emulator.execute();
+    emulator.execute(emu_tx, emu_rx);
+
+    th_handler.join().unwrap();
 }
